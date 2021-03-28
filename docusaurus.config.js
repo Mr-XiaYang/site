e6bb98ec8837a88f1e38b0fa369f36b5eb5863dc
夏阳 <xiayang@xiayangdeMac-mini.local>
2021-03-28 19:25:43 +0800

    修改docusaurus配置.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,97 +1,62 @@
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'My Site',
-  tagline: 'The tagline of my site',
-  url: 'https://your-docusaurus-test-site.com',
+  title: '夏阳',
+  tagline: '我是夏阳，一名兴趣使然的程序员',
+
+  url: 'https://blog.xiayang.space',
   baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'throw',
+  onDuplicateRoutes: 'throw',
+
+  organizationName: 'standout-jjc', // Usually your GitHub org/user name.
+  projectName: 'xiayang-blog', // Usually your repo name.
+
   themeConfig: {
     navbar: {
-      title: 'My Site',
+      title: '夏阳',
       logo: {
-        alt: 'My Site Logo',
+        alt: 'XiaYang Logo',
         src: 'img/logo.svg',
       },
       items: [
-        {
-          to: 'docs/',
-          activeBasePath: 'docs',
-          label: 'Docs',
-          position: 'left',
-        },
-        {to: 'blog', label: 'Blog', position: 'left'},
-        {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
-        },
+        {to: 'blog', label: '博客', position: 'left'},
+        // {to: 'docs/', activeBasePath: 'docs', label: 'Docs', position: 'left'},
+        {href: 'https://github.com/standout-jjc/xiayang-blog', label: 'GitHub', position: 'right'},
       ],
+      hideOnScroll: true
     },
     footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Getting Started',
-              to: 'docs/',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
-        },
-        {
+      links: [{
           title: 'More',
           items: [
-            {
-              label: 'Blog',
-              to: 'blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
+            {label: 'Blog', to: 'blog'},
+            {label: 'GitHub', href: 'https://github.com/standout-jjc/xiayang-blog'},
+            {label: 'Git个人镜像', href: 'https://git.xiayang.space'},
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} 夏阳的个人站 ｜ 鲁ICP备20012782号`,
     },
   },
+
   presets: [
     [
       '@docusaurus/preset-classic',
       {
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
-        },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/standout-jjc/xiayang-blog/edit/main',
+        },
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/standout-jjc/xiayang-blog/edit/main',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
